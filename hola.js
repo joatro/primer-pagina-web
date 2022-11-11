35a020a23c43154299b4849c4be3c4dc3226df3b
Joetro <114252968+Joetro@users.noreply.github.com>
2022-11-10 17:46:06 -0800

    fix css and html ,img

---
+++ hola.js
@@ -0,0 +1,22 @@
+window.addEventListener('load',function(){
+    console.log('el contenido ha cargado!')
+    var imagenes = [];
+    imagenes[0] = 'img/imagen1.jpg';
+    imagenes[1] = 'img/imagen2.jpg';
+    imagenes[2] = 'img/imagen3.jpg';
+   
+
+    var indiceImagenes = 0;
+    var tiempo = 2000;
+    function cambiarImagenes(){
+        
+        document.slider.src = imagenes[indiceImagenes];
+        if(indiceImagenes < 2){
+            indiceImagenes ++;
+        }else{
+            indiceImagenes = 0;
+        }
+    }
+
+    setInterval(cambiarImagenes,tiempo);
+});
