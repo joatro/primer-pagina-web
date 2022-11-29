d42c23055c9b8270dae0c9ba04295cfdab0ee901
Joetro <114252968+Joetro@users.noreply.github.com>
2022-11-29 12:19:28 -0800

    html,js,css

--- hola.js
+++ hola.js
@@ -4,6 +4,12 @@ window.addEventListener('load',function(){
     imagenes[0] = 'img/imagen1.jpg';
     imagenes[1] = 'img/imagen2.jpg';
     imagenes[2] = 'img/imagen3.jpg';
+    imagenes[3] = 'img/imagen4.jpg';
+    imagenes[4] = 'img/imagen5.jpg';
+    imagenes[5] = 'img/imagen6.jpg';
+    imagenes[6] = 'img/imagen7.jpg';
+    imagenes[7] = 'img/imagen8.jpg';
+
    
 
     var indiceImagenes = 0;
