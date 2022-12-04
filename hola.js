e59463a772425275c7fbcaf1c14fc22ebac8c993
Joetro <114252968+Joetro@users.noreply.github.com>
2022-12-03 21:42:42 -0800

    js ftimen

--- hola.js
+++ hola.js
@@ -17,7 +17,7 @@ window.addEventListener('load',function(){
     function cambiarImagenes(){
         
         document.slider.src = imagenes[indiceImagenes];
-        if(indiceImagenes < 2){
+        if(indiceImagenes < 6){
             indiceImagenes ++;
         }else{
             indiceImagenes = 0;
